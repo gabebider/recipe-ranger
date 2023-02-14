b2734ed0a2d1d6bac2807b53351cfb0d8f172269
gabebider <gabebider2024@u.northwestern.edu>
2023-02-13 21:22:39 -0600

    Clarify comments

--- RecipeFinder.py
+++ RecipeFinder.py
@@ -11,24 +11,24 @@ else:
 
 # normalize the input
 # probably need to stop the user from inputting some bad stuff like numbers or others
-recipe_request = recipe_request.lower().strip()
-recipe_request.replace(" ", "+")
+recipe_request = recipe_request.lower().strip() 
+recipe_request.replace(" ", "+") # replace spaces with pluses for url
 
 # search for the recipe on AllRecipescom
 search_url = f"https://allrecipes.com/search?q={recipe_request}"
 search_response = requests.get(search_url)
 search_soup = BeautifulSoup(search_response.text, "html.parser")
-search_list = search_soup.find("div", {"id": "card-list_1-0"})
-search_a_tags = search_list.find_all("a")
+search_list = search_soup.find("div", {"id": "card-list_1-0"}) # find the section of recipes
+search_a_tags = search_list.find_all("a")  # find all the links to recipes
 search_recipes = []
 for a_tag in search_a_tags:
-    title = a_tag.text.strip()
-    title = title.replace("\n", " ")
-    title = title.replace(",", "")
-    title = re.sub(' +', ' ', title)
-    title = re.sub(r'\s[0-9]+\sRatings', '', title)
-    title = title.replace("Save ", "")
-    search_recipes.append((title, a_tag["href"]))
+    title = a_tag.text.strip() # remove leading and trailing whitespace
+    title = title.replace("\n", " ") # relplace newlines with spaces
+    title = title.replace(",", "") # remove commas
+    title = re.sub(' +', ' ', title) # remove extra spaces
+    title = re.sub(r'\s[0-9]+\sRatings', '', title) # remove ratings
+    title = title.replace("Save ", "") # remove "Save" from the beginning of the title
+    search_recipes.append((title, a_tag["href"])) # add the title and url to the list
 
 # Now we have a list of tuples with the recipe name and the url
 # index 0 is the first search result, index 1 is the second, etc.
@@ -51,6 +51,7 @@ else:
     while True:
         while i < len(search_recipes):
             if i < limit:
+                # print the recipe name and number
                 print(f"({option_number}) {search_recipes[i][0]}")
                 choices.add(option_number)  
                 option_number += 1
@@ -63,13 +64,17 @@ else:
                 i += 1
                 break
         recipe_choice = input("Which recipe would you like to cook?: ")
+        # validate the input as a number
         while not recipe_choice.isdigit():
             recipe_choice = input("Please enter a valid number: ")
+        # validate the input as a number in the choices
         while int(recipe_choice) not in choices:
             recipe_choice = input("Please enter a valid number: ")
+        # if the user chose the next 5 results, increment the limit and continue
         if int(recipe_choice) == page_num_choice:
             limit += LIMIT_INCREMENT
             continue
+        # otherwise we have a valid choice
         else:
             recipe_choice = int(recipe_choice) - 1
             break
