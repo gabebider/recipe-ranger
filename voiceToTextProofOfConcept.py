7d2513a3d5430772632842d70827469e0342e5ad
gabebider <gabebider2024@u.northwestern.edu>
2023-02-27 20:52:16 -0600

    better formatting

--- voiceToTextProofOfConcept.py
+++ voiceToTextProofOfConcept.py
@@ -6,25 +6,37 @@ import pyttsx3
 # had to brew install portaudio
 # pip install pyttsx3
 
-# Initialize the text-to-speech engine
-engine = pyttsx3.init()
+def listener():
+    # create a recognizer object
+    r = sr.Recognizer()
 
-# create a recognizer object
-r = sr.Recognizer()
+    # use the default microphone as the audio source
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source)
 
-# use the default microphone as the audio source
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
+    # recognize speech using Google Speech Recognition
+    try:
+        recongized = r.recognize_google(audio, show_all=True)
+        text = recongized['alternative'][0]['transcript']
+        confidence_score = recongized['alternative'][0]['confidence']
+        return text, confidence_score
+    except sr.UnknownValueError:
+        # print("Google Speech Recognition could not understand audio")
+        return None
+    except sr.RequestError as e:
+        # print(f"Could not request results from Google Speech Recognition service; {e}")
+        return None
 
-# recognize speech using Google Speech Recognition
-try:
-    text = r.recognize_google(audio, show_all=True)
-    text = text['alternative'][0]['transcript']
-    print("You said: " + text)
+def reader():
+    # Initialize the text-to-speech engine
+    engine = pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print(f"Could not request results from Google Speech Recognition service; {e}")
+
+if __name__ == "__main__":
+    text, confidence_score = listener()
+    if text is not None:
+        print(text)
+        print(f"confidence_score: {confidence_score}")
+        reader()
