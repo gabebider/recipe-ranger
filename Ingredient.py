bf5415a02ec633eac1e48be213e58de94ce43f47
srothfleisch <spencerrothfleisch2024@u.northwestern.edu>
2023-07-13 19:14:48 -0400

    fixed this thing to actually work again (how did larry not notice it didnt)

--- Ingredient.py
+++ Ingredient.py
@@ -1,12 +1,12 @@
 from utils import parseIngredientCore
 import spacy
-import datetime as dt
+
 class Ingredient():
-    def __init__(self, name:str, quantity:str, unit:str,
+    def __init__(self, name, quantity, unit,
                 nlp:spacy.language.Language):
-        assert isinstance(name, str), "Ingredient name must be a string"
-        assert isinstance(quantity, str), "Ingredient quantity must be a string"
-        assert isinstance(unit, str), "Ingredient unit must be a string"
+        #assert isinstance(name, str), "Ingredient name must be a string"
+        #assert isinstance(quantity, str), "Ingredient quantity must be a string"
+        #assert isinstance(unit, str), "Ingredient unit must be a string"
         assert isinstance(nlp, spacy.language.Language), "nlp must be a spacy language object"
 
         self.name = name.replace("(Optional)","").strip()
@@ -25,9 +25,9 @@ class Ingredient():
             self.preparations = []
     
     def __str__(self):
-        if self.quantity == "" and self.unit == "":
+        if self.quantity == None and self.unit == None:
             return self.name
-        elif self.unit == "":
+        elif self.unit == None:
             return f"{self.quantity} {self.name}"
         elif isinstance(self.unit, list) and isinstance(self.quantity, list):
             return f"{self.quantity[0]} {self.unit[0]} and/of {self.quantity[1]} {self.unit[1]} {self.name}"
